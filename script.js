373486c5f1e8f3078d61694dff38cc792ee43d7c
dblasko <daniel.blasko.dev@gmail.com>
2020-03-25 16:27:44 +0100

    done implementing first version of main menu

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function loadMenu(event) {
-    console.log("load");
-    let template = document.getElementById("menuTemplate");
-    let clone = document.importNode(template.content, true);
+const MENU = 'M';
+const GAME = 'G';
+const welcomeQuotes = [" : tu ne m'auras pas !", " : ces vacances, je voyage à ta place 🌎", " : t'es sûr qu'il t'en reste assez ? 🧻"];
 
-    document.getElementsByTagName("body")[0].appendChild(clone);
+function cleanContentZone() {
+    document.getElementById("contentZone").innerHTML = "";
 }
+
+function loadScreen(screen) { // screen is either the MENU or GAME constant
+    cleanContentZone();
+    
+    let templateId = (screen === GAME)? "gameTemplate" : "menuTemplate";
+
+    let template = document.getElementById(templateId);
+    let clone = document.importNode(template.content, true);
+    document.getElementById("contentZone").appendChild(clone);
+}
+
+
+function loadMenu(event) {
+    // EVENTUELLEMENT RESET VARIABLES DE JEU ICI OU LOADGAME
+    let quote = welcomeQuotes[Math.floor((Math.random()*welcomeQuotes.length))];
+    document.getElementById("quote").innerText = quote;
+    
+    loadScreen(MENU);
+}
+
+function loadGame(event) {
+    document.getElementById("quote").innerText = " ⬅ Le virus te ramène au menu !";
+    loadScreen(GAME);
+}
+
