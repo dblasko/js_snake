fd450e5a9f27b411a43b332f1fe752f9c1293726
dblasko <daniel.blasko.dev@gmail.com>
2020-03-27 23:46:44 +0100

    added error popups for a better UX

--- scripts/gameState.js
+++ scripts/gameState.js
@@ -7,8 +7,8 @@ let gameState = (function() {
     // f° getLevel => choppe dans l'URL? avec le # cf le TP 
     // sinon au clic dans une var globale, ou un setter gameState => getLevel l'utilise
 
-    let SNAKE = [];
-    let WORLD = [
+    let snake = [];
+    let world = [
         [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
         [EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY, EMPTY],
         [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
@@ -17,8 +17,28 @@ let gameState = (function() {
         [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
     ];
     // L’information sur le serpent est donc stockée de manière redondante dans ces deux tableaux. Cette redondance rendra la gestion du jeu plus simple par la suite.
+    let currentLevel;
     let key;
 
+    function notifyLevelSelected(level) {
+        fetch(`./assets/levels/${level}.json`).then(response=>{
+            if (response.ok) {
+                response.json().then(resp => {
+                    currentLevel = resp;
+                    startGame();
+                });
+            } else {
+                console.log("Ce niveau n'existe pas !");
+                viewHandler.popMessage("Je ne peux pas infecter ce pays !");
+            }
+        });
+    }
+
+    function startGame() { // private 
+        viewHandler.loadScreen(viewHandler.GAME); // TODO : afficher après draw ?
+        // initial draw (random apple...) + initial direction ... etc + delay step()
+        // BIEN REMETTRE A 0 TOUTES VAL DE JEU ICI
+    }
 
     function setKey(pressedKey) {
         if (validKeys.includes(pressedKey)) key = pressedKey;
@@ -36,6 +56,7 @@ let gameState = (function() {
     return { // public attributes / methods
         setKey: setKey,
         step: step,
+        notifyLevelSelected: notifyLevelSelected,  
     };
 }());
 
@@ -44,4 +65,14 @@ document.addEventListener("keydown", (event) => {
     gameState.setKey(event.key);
 });
 
-// TODO : delay step 
+
+
+// TODO !!! : SUPPR LA FONCTION STEP MISE EN TIMEOUT
+
+// ACCUEIL : listener avec ids sur la liste niveaux
+// au clic : appelle loadLevel de gameState avec en param° le nom du lvl (id)
+// gameState appelle AJAX pour récupérer le JSON en objet, qu'il stocke en attr currentLevel
+
+
+    // set le gameState, le laisser fetch puis loadGame lui même ? il fait viewHandler...
+    // BIEN GERER SI PAS UN DES LVL
--- scripts/viewHandler.js
+++ scripts/viewHandler.js
@@ -33,11 +33,20 @@ let viewHandler = (function() {
         updateWelcomeQuote();
     }
 
-    return {  // public attributs/methods, the reste is private 
+    function popMessage(msg) {  // to show an error msg for 3 seconds
+        var snackbar = document.getElementById("snackbar");
+        snackbar.className = "show";
+        snackbar.innerHTML = msg;
+        snackbar.hidden = false;
+        setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); snackbar.hidden=true; }, 3000);
+    }
+
+    return {  // public attributs/methods, the rest is private 
         MENU: MENU,
         GAME: GAME,
 
         loadScreen: loadScreen,
+        popMessage: popMessage,
     };
 }());
 
@@ -46,6 +55,6 @@ function loadMenu(event) {
     viewHandler.loadScreen(viewHandler.MENU);
 }
 
-function loadGame(event) {
-    viewHandler.loadScreen(viewHandler.GAME);
+function levelSelected(event) {
+    gameState.notifyLevelSelected(event.target.id);
 }
