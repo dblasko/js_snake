3b09950bc41af7e40072038419ab3ac9a05051b1
dblasko <daniel.blasko.dev@gmail.com>
2020-03-27 22:18:33 +0100

    updated readme.md

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# JS Snake
+# JS Snake 🐍
 A practical project for my JS course.
 
-All creative choices have to be taken with a grain of salt and are not meant to offend anyone.
+## Disclaimer
+
+🛑 All creative choices have to be taken with a grain of salt and are not meant to offend anyone. 🛑
+
+## Instructions 
 
 **Instructions are available [here](https://perso.liris.cnrs.fr/pierre-antoine.champin/enseignement/intro-js/s6.html).**
+
+## Running the project 💻
+
+* Clone the project on your machine.
+* Open a terminal at the root of the project folder.
+* Run `python3 -m http.server` if you have Python 3 installed. Else, use any web server you prefer. 
+* The project is then available at [](0.0.0.0:8000). Have fun ! 🐍
--- scripts/gameState.js
+++ scripts/gameState.js
@@ -1,4 +1,5 @@
 let gameState = (function() {
+    const validKeys = ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"];
     const EMPTY = 'E';
     const SNAKE = 'S';
     const FOOD = 'F';
@@ -12,13 +13,35 @@ let gameState = (function() {
         [EMPTY, EMPTY, EMPTY, EMPTY,  EMPTY, EMPTY],
         [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
         [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-        [EMPTY, EMPTY, SNAKE, EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
         [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
     ];
     // L’information sur le serpent est donc stockée de manière redondante dans ces deux tableaux. Cette redondance rendra la gestion du jeu plus simple par la suite.
+    let key;
 
 
-    return {
+    function setKey(pressedKey) {
+        if (validKeys.includes(pressedKey)) key = pressedKey;
+    }
 
+    function step() {
+        // 1. modif° direction si besoin 
+        // 2. tête rencontre nourriture / mur / morceau corps ?
+            // aug° score + nourriture dans case vide aléatoire 
+            // Sinon, fin partie : score + menu 
+        // MAJ tab SNAKE -> avancer (cf énoncé) + aug° taille si mangé (= pas réduire queue) + MAJ WORLD
+        // effacer & redessiner canvas en corresp° avec WORLD
+    }
+
+    return { // public attributes / methods
+        setKey: setKey,
+        step: step,
     };
 }());
+
+
+document.addEventListener("keydown", (event) => {
+    gameState.setKey(event.key);
+});
+
+// TODO : delay step 
